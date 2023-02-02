f9e2811a97d6c8a6bcec404226502eae94b0e250
Garpusha <theflash@yandex.ru>
2023-02-02 15:03:05 +0300

    get_user_photos

--- main.py
+++ main.py
@@ -52,14 +52,26 @@ class VK:
                   }
         res = requests.get(url, params=params)
         return res.json()
+    def get_user_photos(self, user_id):
+        url = 'https://api.vk.com/method/photos.get'
+        params = {'owner_id': user_id,
+                  'album_id': 'profile',
+                  'extended': 1,
+                  'access_token': self.token,
+                  'v': '5.131'
+                  }
+        res = requests.get(url, params=params)
+        return res.json()
+
 
 if __name__ == "__main__":
     yandex_token = read_config('tokens.ini', 'Tokens', 'YandexToken')
     vk_token = read_config('tokens.ini', 'Tokens', 'VKToken')
     directory = read_config('config.ini', 'Path', 'Directory')
-    vk_id = ('config.ini', 'ID', 'VK_ID')
+    vk_id = read_config('config.ini', 'ID', 'VK_UserID')
     # my_yandex = YandexDisk(yandex_token)
     # my_yandex.make_dir(directory)
     # print(my_yandex.upload_by_url('https://img2.goodfon.ru/original/1366x768/f/f7/kotyata-ryzhie-podstavka.jpg', directory))
     my_vk = VK(vk_token)
-    pprint(my_vk.get_user_info(vk_id))
+    # pprint(my_vk.get_user_info(vk_id))
+    pprint(my_vk.get_user_photos(vk_id))
